cee4ca7b8898519805a13bd60aa832bafff9e818
Stephen Cook <stephen@stephencookdev.co.uk>
2019-12-20 19:50:22 +0000

    Initial gatsby setup

---
+++ src/pages/index.md
@@ -0,0 +1,7 @@
+---
+templateKey: index-page
+title: Welcome to Moonfleet!
+tagline: Stay at Moonfleet, a Grade II listed former manor house, situated 300 yards from the sea in Skinningrove, a traditional fishing village.
+body: >
+  Hey this should be _markdown_.
+---
---
+++ src/templates/index-page.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+export const IndexPageTemplate = ({
+  title,
+  tagline,
+  carousel,
+  body,
+}) => (
+  <div>
+      <h1>{title}</h1>
+      <h2>{tagline}</h2>
+      <p>{body}</p>
+  </div>
+)
+
+IndexPageTemplate.propTypes = {
+  title: PropTypes.string,
+  tagline: PropTypes.string,
+  carousel: PropTypes.arrayOf(PropTypes.shape({ image: PropTypes.string })),
+  description: PropTypes.string,
+}
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <IndexPageTemplate
+        {...frontmatter}
+      />
+    </Layout>
+  )
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    markdownRemark {
+      frontmatter {
+        title
+        tagline
+        body
+      }
+    }
+  }
+`
