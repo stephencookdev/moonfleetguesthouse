fe5f5bc621dd5373063fe1594856edd9c6ffdee5
Stephen Cook <stephen@stephencookdev.co.uk>
2019-12-23 17:09:38 +0000

    Fix up the mobile styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,39 @@
 import { useStaticQuery, graphql, Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.css";
 
-const Nav = ({ links }) => (
-  <nav>
-    {links.map(({ title, href, highlight }) => (
-      <Link
-        key={href}
-        to={href}
-        className={highlight ? styles.highlight : null}
-      >
-        {title}
-      </Link>
-    ))}
-  </nav>
-);
+const Nav = ({ links }) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div
+      className={[styles.navContainer, active && styles.active]
+        .filter(Boolean)
+        .join(" ")}
+    >
+      <button className={styles.hamburger} onClick={() => setActive(!active)} />
+
+      <nav>
+        {links.map(({ title, href, highlight }) => (
+          <Link
+            key={href}
+            to={href}
+            className={highlight ? styles.highlight : null}
+          >
+            {title}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+};
 
 Nav.propTypes = {
   links: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
-const Header = ({ siteTitle, location }) => {
+const Header = ({ location }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
